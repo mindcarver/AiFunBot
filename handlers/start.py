d22352c0ed254332db0cb3f7d00b7fe2dfe179ef
mindcarver <626984947@qq.com>
2024-07-01 12:30:48 +0800

    perf:conversation end status

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,6 @@
 # handlers/start.py
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, ConversationHandler
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -42,3 +42,9 @@ async def start(update: Update, context: CallbackContext) -> None:
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text(welcome_message, reply_markup=reply_markup)
+
+
+async def cancel_handler(update, context) -> int:
+    """取消当前会话并通知用户"""
+    await update.message.reply_text("会话已取消。随时可以使用按钮开始新的任务。")
+    return ConversationHandler.END  # 结束当前的Conversation
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from telegram.ext import (
     filters,
 )
 from config import TOKEN
-from handlers.start import start
+from handlers.start import start,cancel_handler
 from handlers.button_callback import button_callback
 from handlers.generator import handle_image_generation
 from constant import IMAGE_GENERATION
@@ -35,12 +35,12 @@ def main() -> None:
         states={
             IMAGE_GENERATION: [
                 MessageHandler(
-                    filters.TEXT & ~filters.COMMAND,
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")), ## 输入Done 取消
                     handle_image_generation
                 )
             ],
         },
-        fallbacks=[],
+        fallbacks=[CommandHandler('cancel', cancel_handler)], ## 通过取消的方式退出对话
     )
     application.add_handler(start_handler)
     ## application.add_handler(CallbackQueryHandler(button_callback)) ## 这个会影响下面的会话内容，不会生效
